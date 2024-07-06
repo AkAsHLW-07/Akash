d1a79fb38347aa551e84b5e0b486dce2bed7baab
AkAsHLW-07 <akashrugma2020@gmail.com>
2024-07-06 20:11:04 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+def cal():
+    
+    a=int(input("Enter a number"))
+    b=int(input("Enter a number"))
+    c=(input("Enter an operation"))
+    if c=="+":
+        print("the sum is:",a+b)    
+    if c=="-":
+        print("the difference is:",a-b)
+    if c=="*":
+        print("the product is:",a*b)
+    if c=="/":
+        print("the quotient is:",a/b)
+cal()
+    
+
