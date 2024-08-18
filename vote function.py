93b0e69d9b309a86511a30aa39a19caae7401c90
AkAsHLW-07 <akashrugma2020@gmail.com>
2024-08-18 20:09:46 +0530

    Add files via upload

---
+++ vote function.py
@@ -0,0 +1,12 @@
+#akash
+def voting():
+    a=int(input("Enter your age"))
+    if a==18 and a>18:
+        print("You are eligible to vote")
+    else:
+        print(" You are Not eligible to vote")
+      
+voting()
+        
+    
+
